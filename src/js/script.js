413b0fae547eecca0c64514b9501d8bfb21dbcb1
ARESKAN2108 <maxareskan@gmail.com>
2024-01-10 00:14:17 +0300

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,16 @@ $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1000,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/right.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
 });
